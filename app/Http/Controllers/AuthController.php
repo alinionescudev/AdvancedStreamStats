4b9436b267f76494a812737ab1bf817a358a23a5
Alin Ionescu | eMAG, Technology <alin.ionescu@emag.ro>
2022-12-04 21:03:05 +0200

    add auth and register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+  
+namespace App\Http\Controllers;
+  
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\User;
+use Hash;
+  
+class AuthController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('auth.login');
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $remember = $request->input('remember');
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials, $remember)) {
+            $user = Auth::user();
+
+            return redirect()->intended('dashboard')
+                    ->withSuccess('You have Successfully logged in, ' . $user->name);
+        }
+  
+        return redirect("login")->withErrors('Oppes! You have entered invalid credentials');
+    }
+
+    public function postRegistration(Request $request)
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+    
+    public function dashboard()
+    {
+        if (Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('Opps! You do not have access');
+    }
+
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }
+
+    public function logout() {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('login');
+    }
+}
